afc517f9ed0e5e33695c3cf855bd4dfc726c8f25
Michael Shi <1027michael@gmail.com>
2017-05-13 09:37:00 -0400

    Message

--- src/icebot/Bot.java
+++ src/icebot/Bot.java
@@ -5,17 +5,18 @@ import javax.security.auth.login.*;
 import net.dv8tion.jda.core.*;
 import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.managers.Presence;
 
 public class Bot {
 
 	public static JDA jda;
 	
 	public static final String BOT_TOKEN = "MzEyNDA2MTYyMzQ5ODgzMzkz.C_amxQ.RslY0c9MfaDHspPC1nfY-CvJkNo";
-	 
+	public static final String GAME = "Paragon";
+	
 	public static void main(String[] args){
 		System.out.println("Welcome!"); //harry added this
 		
-		jda.getPresence().setGame(Game.of("Paragon"));
 		System.out.println("[JDA] Game updated.");
 		try{ //initialization
 		jda = new JDABuilder(AccountType.BOT).addEventListener(new BotListener()).setToken(BOT_TOKEN).buildAsync();
@@ -23,5 +24,6 @@ public class Bot {
 		catch(LoginException | IllegalArgumentException | RateLimitedException e){
 			e.printStackTrace();
 		}
+		jda.getPresence().setGame(Game.of(GAME));
 	}
 }
