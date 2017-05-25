21a8e2892178d2f52331db221c101904b59c7c89
Michael Shi <1027michael@gmail.com>
2017-05-25 19:43:56 -0400

    Message

--- src/icebot/Bot.java
+++ src/icebot/Bot.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class Bot {
-
+	//"PROOF"
 	public static JDA jda;
 	
 	public static String BOT_TOKEN = "";
